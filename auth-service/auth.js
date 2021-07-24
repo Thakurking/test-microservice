6decabea939b7bccf0c9af94693f0efa9e1cfc4d
Ritesh Thakur <riteshkumar3310@gmail.com>
2021-07-24 13:27:09 +0530

    made some changes

--- auth-service/auth.js
+++ auth-service/auth.js
@@ -1,12 +1,10 @@
 const token = require("jsonwebtoken");
 module.exports = async (req, res) => {
-  try {
-    const jwt = req.cookies.token;
-    console.log(jwt);
-    if (!jwt) {
-      return res.json({ message: "token not found" });
-    }
-  } catch (error) {
-    return res.json({ message: "Access failed" });
+  const jwt = req.cookies.token;
+  console.log(jwt);
+  if (!jwt) {
+    return res.json({ message: "token not found" });
+  } else {
+    return res.json({ message: "Please Provide Token" });
   }
 };
